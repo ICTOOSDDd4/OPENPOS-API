4b38ce34ac4c8380959e0acbd476978e2899838a
Tymen Vis <Tymenvis@outlook.com>
2022-12-03 17:37:19 +0100

    Setup for EventListener for newOrder

---
+++ OPENPOS-API/Controllers/OrderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using OpenPOS_API.Models;
+
+namespace OPENPOS_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IHubContext<EventHub> _hubContext;
+
+        public OrderController(IHubContext<EventHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            await _hubContext.Clients.All.SendAsync("newOrder", new Order() {Id = 59});
+            return Ok("Success");
+        }
+    }
+}
---
+++ OPENPOS-API/EventHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.SignalR;
+
+namespace OPENPOS_API
+{
+    public class EventHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Debug.WriteLine(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+    }
+}
