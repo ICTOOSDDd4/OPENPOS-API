259103365d3fbcb06dc8def5b5bb9346336a5f0b
Tymen Vis <Tymenvis@outlook.com>
2022-12-05 00:21:33 +0100

    Improving code

---
+++ OPENPOS-API/Events.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace OPENPOS_API
+{
+    public class Events
+    {
+        public static void MapEvents(WebApplication app)
+        {
+            app.MapHub<EventHub>("/event_hub");
+            app.MapHub<TikkieEventHub>("/tikkie_event");
+        }
+    }
+}
--- OPENPOS-API/Program.cs
+++ OPENPOS-API/Program.cs
@@ -20,26 +20,22 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.Use(async (context, next) =>
-{
-    AuthorizationMiddleware authorization = new AuthorizationMiddleware(next);
-    if (context.Request.Path == "/event_hub" ||
-        context.Request.Path == "/api/order")
-    {
-        await authorization.Invoke(context, builder);
-    }
-    else
-    {
-        await next(context);
-    }
-});
+app.UseMiddleware<AuthorizationMiddleware>();
+//app.Use(async (context, next) =>
+//{
+//    AuthorizationMiddleware authorization = new AuthorizationMiddleware(next);
+//    if (context.Request.Path == "/event_hub" ||
+//        context.Request.Path == "/api/order")
+//    {
+//        await authorization.Invoke(context, builder);
+//    }
+//    else
+//    {
+//        await next(context);
+//    }
+//});
 
 app.MapControllers();
-
-app.MapHub<EventHub>("/event_hub");
-
-app.MapHub<TikkieEventHub>("/tikkie_event");
-
 
 app.UseHttpsRedirection();
 
