20a400a5d4794a92bd99d0c8403629ea830310f5
JMC-Cyber <jmccrans@gmail.com>
2022-12-07 15:04:28 +0100

    Update Events.cs

--- OPENPOS-API/Events.cs
+++ OPENPOS-API/Events.cs
@@ -5,7 +5,7 @@
         public static void Initialize(WebApplication app)
         {
             app.MapHub<OrderEventHub>("/order_event");
-
+            // Different routes for different hubs
             app.MapHub<TikkieEventHub>("/tikkie_event");
         }
     }
